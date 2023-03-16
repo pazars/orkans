0fda0407edd9427e8aaf3596118ff100596071aa
Davis Pazars <davis.pazars@protonmail.com>
2023-03-16 11:53:19 +0200

    Reduce test accuracy threshold

--- tests/test_pysteps_opera.py
+++ tests/test_pysteps_opera.py
@@ -43,7 +43,7 @@ def test_pysteps_steps():
         if type(value) == str:
             assert str(res[key]) == str(value)
         elif type(value) == float:
-            assert abs(res[key] - value) < 1e-5
+            assert abs(res[key] - value) < 1e-4
 
 
 def test_pysteps_anvil():
@@ -69,7 +69,7 @@ def test_pysteps_anvil():
         if type(value) == str:
             assert str(res[key]) == str(value)
         elif type(value) == float:
-            assert abs(res[key] - value) < 1e-5
+            assert abs(res[key] - value) < 1e-4
 
 
 def test_pysteps_sseps():
@@ -98,7 +98,7 @@ def test_pysteps_sseps():
         if type(value) == str:
             assert str(res[key]) == str(value)
         elif type(value) == float:
-            assert abs(res[key] - value) < 1e-5
+            assert abs(res[key] - value) < 1e-4
 
 
 def test_pysteps_linda():
@@ -125,4 +125,4 @@ def test_pysteps_linda():
         if type(value) == str:
             assert str(res[key]) == str(value)
         elif type(value) == float:
-            assert abs(res[key] - value) < 1e-5
+            assert abs(res[key] - value) < 1e-4
